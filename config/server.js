d8b5888fb28c22a53cadce9df06ebb3494d8ce8f
jenarolopez <jenjenlopez37@gmail.com>
2021-03-05 17:13:52 +0800

    sdasd

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'f879f6b025dc47284da86e7f28770e52'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '74454e55-d74b-4b52-9153-85895ae3a7b4'
+};
