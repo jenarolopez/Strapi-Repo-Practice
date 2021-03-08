1dcb5271dd835f83c56a4fad6185d7d4e2810744
jenarolopez <jenjenlopez37@gmail.com>
2021-03-08 16:34:56 +0800

    last try

--- config/database.js
+++ config/database.js
@@ -1,37 +1,14 @@
-const parse = require('pg-connection-string').parse;
-
-module.exports = ({ env }) => {
-
-  if(env('NODE_ENV') === 'production'){
-    return {
-      defaultConnection: 'default',
-      connections: {
-        default: {
-          connector: 'mongoose',
-          settings: {
-            uri: env('DATABASE_URI'),
-          },
-          options: {
-            ssl: true,
-          },
-        },
+module.exports = ({ env }) => ({
+  defaultConnection: 'default',
+  connections: {
+    default: {
+      connector: 'mongoose',
+      settings: {
+        uri: env('DATABASE_URI'),
       },
-    }
-  }
-  
-  return {
-    defaultConnection: 'default',
-    connections: {
-      default: {
-        connector: 'bookshelf',
-        settings: {
-          client: 'sqlite',
-          filename: env('DATABASE_FILENAME', '.tmp/data.db'),
-        },
-        options: {
-          useNullAsDefault: true,
-        },
+      options: {
+        ssl: true,
       },
     },
-  }
-};
+  },
+});
