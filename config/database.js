dddcd429965d36f0c9ca42a5d0e660b91d81e5c6
jenarolopez <jenjenlopez37@gmail.com>
2021-03-08 16:15:51 +0800

    strapi fix v2

--- config/database.js
+++ config/database.js
@@ -3,22 +3,16 @@ const parse = require('pg-connection-string').parse;
 module.exports = ({ env }) => {
 
   if(env('NODE_ENV') === 'production'){
-    const config = parse(process.env.DATABASE_URL);
     return {
       defaultConnection: 'default',
       connections: {
         default: {
-          connector: 'bookshelf',
+          connector: 'mongoose',
           settings: {
-            client: 'postgres',
-            host: config.host,
-            port: config.port,
-            database: config.database,
-            username: config.user,
-            password: config.password,
+            uri: env('DATABASE_URI'),
           },
           options: {
-            ssl: false,
+            ssl: true,
           },
         },
       },
